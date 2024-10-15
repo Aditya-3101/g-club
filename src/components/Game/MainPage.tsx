cf264075413826f3ec5c36d84538a96479f026cc
Aditya-3101 <adityadhayfule3101@gmail.com>
2024-10-15 23:54:04 +0530

    fixed UI breakpoints issues

--- src/components/Game/MainPage.tsx
+++ src/components/Game/MainPage.tsx
@@ -143,7 +143,7 @@ export const MainPage:React.FC = () => {
 
     return(
         <section className='bg-[#101014] flex flex-col  items-center py-4'>
-            <div className='w-[80%] sm:w-[80%] mx-auto sm:py-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center sm:gap-4 md:gap-y-8 gap-8'>
+            <div className='w-[80%] sm:w-[80%] xl:w-[70%] mx-auto sm:py-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center sm:gap-4 md:gap-y-8 gap-8'>
             {(data||[]).concat(moreData||[]).map((para,index)=>{
                   return <Link to={`/games/${para.name}`} key={index} className='w-[16rem] md:w-[18rem]'>
                   <ReleaseCard name={para.name} price={para.price} orgPrice={para.orgPrice} imgUrl={para.imgUrl} type="gamesPage" />
--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -5,7 +5,7 @@ import { Footer } from '../Home/Footer.tsx'
 import { Suspense } from 'react'
 
 export const HomeLayout:React.FC = () => {
-    return<main className='App'>
+    return<main className='App max-w-[1440px] mx-auto'>
         <Navbar/>
         <Suspense fallback={<h1 className='p-4 text-xl text-slate-50 w-full bg-[#101014] '>Loading.....</h1>}>
         <Outlet/>
